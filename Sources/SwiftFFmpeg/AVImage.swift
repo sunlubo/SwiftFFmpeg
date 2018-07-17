fd2850029c39cdd81f8ee3ce8e29351391630c2b
sunlubo <sunlubo.sun@gmail.com>
2018-07-17 19:48:36 +0800

    add av_image_* & sws_* api

---
+++ Sources/SwiftFFmpeg/AVImage.swift
@@ -0,0 +1,36 @@
+//
+//  AVImage.swift
+//  SwiftFFmpeg
+//
+//  Created by sunlubo on 2018/7/12.
+//
+
+import CFFmpeg
+
+public final class AVImage {
+
+    /// Allocate an image with size w and h and pixel format pix_fmt, and fill pointers and linesizes accordingly.
+    ///
+    /// - Parameters:
+    ///   - buf: buf
+    ///   - linesizes: linesizes
+    ///   - width: width
+    ///   - height: height
+    ///   - pixFmt: pixFmt
+    ///   - align: the value to use for buffer size alignment
+    /// - Returns: the size in bytes required for the image buffer, a negative error code in case of failure
+    public static func alloc(
+        buf: UnsafeMutablePointer<UnsafeMutablePointer<UInt8>?>,
+        linesizes: UnsafeMutablePointer<Int32>,
+        width: Int,
+        height: Int,
+        pixFmt: AVPixelFormat,
+        align: Int32 = 0
+    ) -> Int32 {
+        return av_image_alloc(buf, linesizes, Int32(width), Int32(height), pixFmt, align)
+    }
+
+    public static func free(_ ptr: UnsafeMutableRawPointer) {
+        av_freep(ptr)
+    }
+}
---
+++ Sources/SwiftFFmpeg/SwsContext.swift
@@ -0,0 +1,115 @@
+//
+//  SwsContext.swift
+//  SwiftFFmpeg
+//
+//  Created by sunlubo on 2018/7/11.
+//
+
+import CFFmpeg
+
+public final class SwsContext {
+    internal let ctx: OpaquePointer
+    
+    /// Allocate an empty `SwsContext`.
+    public init() {
+        ctx = sws_alloc_context()
+    }
+    
+    /// Allocate and return an `SwsContext`.
+    ///
+    /// - Parameters:
+    ///   - srcW: the width of the source image
+    ///   - srcH: the height of the source image
+    ///   - srcFormat: the source image format
+    ///   - dstW: the width of the destination image
+    ///   - dstH: the height of the destination image
+    ///   - dstFormat: the destination image format
+    ///   - flags: specify which algorithm and options to use for rescaling
+    public init?(
+        srcW: Int,
+        srcH: Int,
+        srcFormat: AVPixelFormat,
+        dstW: Int,
+        dstH: Int,
+        dstFormat: AVPixelFormat,
+        flags: Int32
+    ) {
+        guard let ptr = sws_getContext(
+            Int32(srcW),
+            Int32(srcH),
+            srcFormat,
+            Int32(dstW),
+            Int32(dstH),
+            dstFormat,
+            flags,
+            nil,
+            nil,
+            nil
+        ) else {
+            return nil
+        }
+        ctx = ptr
+    }
+    
+    /// Scale the image slice in srcSlice and put the resulting scaled slice in the image in dst.
+    ///
+    /// A slice is a sequence of consecutive rows in an image.
+    ///
+    /// Slices have to be provided in sequential order, either in top-bottom or bottom-top order.
+    /// If slices are provided in non-sequential order the behavior of the function is undefined.
+    ///
+    /// - Parameters:
+    ///   - srcSlice: the array containing the pointers to the planes of the source slice
+    ///   - srcStride: the array containing the strides for each plane of the source image
+    ///   - srcSliceY: the position in the source image of the slice to process, that is the number
+    ///     (counted starting from zero) in the image of the first row of the slice
+    ///   - srcSliceH: the height of the source slice, that is the number of rows in the slice
+    ///   - dst: the array containing the pointers to the planes of the destination image
+    ///   - dstStride: the array containing the strides for each plane of the destination image
+    /// - Returns: the height of the output slice
+    public func scale(
+        srcSlice: UnsafePointer<UnsafePointer<UInt8>?>,
+        srcStride: UnsafePointer<Int32>,
+        srcSliceY: Int32,
+        srcSliceH: Int32,
+        dst: UnsafePointer<UnsafeMutablePointer<UInt8>?>,
+        dstStride: UnsafePointer<Int32>
+    ) -> Int32 {
+        return sws_scale(ctx, srcSlice, srcStride, srcSliceY, srcSliceH, dst, dstStride)
+    }
+    
+    /// Returns a Boolean value indicating whether the pixel format is a supported input format.
+    ///
+    /// - Parameter pixFmt: pixel format
+    /// - Returns: true if it is supported; otherwise false.
+    public static func isSupportedInput(_ pixFmt: AVPixelFormat) -> Bool {
+        return sws_isSupportedInput(pixFmt) > 0
+    }
+    
+    /// Returns a Boolean value indicating whether the pixel format is a supported output format.
+    ///
+    /// - Parameter pixFmt: pixel format
+    /// - Returns: true if it is supported; otherwise false.
+    public static func isSupportedOutput(_ pixFmt: AVPixelFormat) -> Bool {
+        return sws_isSupportedOutput(pixFmt) > 0
+    }
+    
+    /// Returns a Boolean value indicating whether an endianness conversion is supported.
+    ///
+    /// - Parameter pixFmt: pixel format
+    /// - Returns: true if it is supported; otherwise false.
+    public static func isSupportedEndiannessConversion(_ pixFmt: AVPixelFormat) -> Bool {
+        return sws_isSupportedEndiannessConversion(pixFmt) > 0
+    }
+    
+    deinit {
+        sws_freeContext(ctx)
+    }
+}
+
+extension SwsContext: AVOptionProtocol {
+    
+    public var objPtr: UnsafeMutableRawPointer {
+        return UnsafeMutableRawPointer(ctx)
+    }
+}
