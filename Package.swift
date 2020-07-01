610df399659c06e01245e21560236446d845b5d2
sunlubo <sunlubo.sun@gmail.com>
2020-07-01 10:43:10 +0800

    Examples -> SwiftFFmpegExamples

--- Package.swift
+++ Package.swift
@@ -4,30 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftFFmpeg",
-    products: [
-        .library(
-            name: "SwiftFFmpeg",
-            targets: ["SwiftFFmpeg"]
-        )
-    ],
-    targets: [
-        .systemLibrary(
-            name: "CFFmpeg",
-            pkgConfig: "libavformat"
-        ),
-        .target(
-            name: "SwiftFFmpeg",
-            dependencies: ["CFFmpeg"]
-        ),
-        .target(
-            name: "SwiftFFmpegExamples",
-            dependencies: ["SwiftFFmpeg"],
-            path: "Sources/Examples"
-        ),
-        .testTarget(
-            name: "SwiftFFmpegTests",
-            dependencies: ["SwiftFFmpeg"]
-        )
-    ]
+  name: "SwiftFFmpeg",
+  products: [
+    .library(
+      name: "SwiftFFmpeg",
+      targets: ["SwiftFFmpeg"]
+    )
+  ],
+  targets: [
+    .systemLibrary(
+      name: "CFFmpeg",
+      pkgConfig: "libavformat"
+    ),
+    .target(
+      name: "SwiftFFmpeg",
+      dependencies: ["CFFmpeg"]
+    ),
+    .target(
+      name: "SwiftFFmpegExamples",
+      dependencies: ["SwiftFFmpeg"]
+    ),
+    .testTarget(
+      name: "SwiftFFmpegTests",
+      dependencies: ["SwiftFFmpeg"]
+    ),
+  ]
 )
