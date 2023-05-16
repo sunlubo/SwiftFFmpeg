6db56263d8eb3a3cb0fb47d3c1d320bacc4d3549
sun_ <sunlubo.sun@gmail.com>
2023-05-16 18:59:30 +0800

    remove AVIO

--- Sources/SwiftFFmpegExamples/main.swift
+++ Sources/SwiftFFmpegExamples/main.swift
@@ -15,7 +15,6 @@ if CommandLine.argc < 2 {
 
     SUBCOMMANDS:
       avio_dir_cmd        API example program to show how to manipulate resources accessed through AVIOContext.
-      avio_reading        API example program to show how to read from a custom buffer accessed through AVIOContext.
       decode_video        video decoding with libavcodec API example
       decode_audio        audio decoding with libavcodec API example
       demuxing_decoding   API example program to show how to read frames from an input file.
@@ -49,8 +48,6 @@ let subcommand = CommandLine.arguments[1]
 switch subcommand {
 case "avio_dir_cmd":
   try avio_dir_cmd()
-case "avio_reading":
-  try avio_reading()
 case "decode_video":
   try decode_video()
 case "decode_audio":
